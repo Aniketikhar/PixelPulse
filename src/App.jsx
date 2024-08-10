2c53bb5dd39501a6ad0943bc3dc17c15314e9baa
Aniketikhar <aniketikhar88@gmail.com>
2024-08-10 23:28:43 +0530

    defining structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
-      <div className='App'>
-        
+      <div className="App">
+        <Navbar />
+        <div className="container d-flex flex-column-reverse flex-md-row">
+          <div className="grid">grid</div>
+          <div className="content">
+            <div className="headline text-white">
+              <h1>Where pixels dance and colors come alive!</h1>
+              <p>
+                PixelPulse transforms text into a captivating visual experience.
+                Watch as characters flow seamlessly across a dynamic grid, with
+                each letter rendered as a pixelated pattern. With every passing
+                moment, colors shift randomly, bringing a vibrant, ever-changing
+                spectacle to your screen.{" "}
+              </p>
+              <p>
+                Type a message below and see the grid of pixels change
+                accordingly:
+              </p>
+            </div>
+            <form >
+            <input
+              type="text"
+              name="input"
+              className=" bg-black text-white border-0 px-3 py-2 rounded-2 outline-none w-50 "
+              
+              placeholder="Enter text here..."
+              required
+              id=""
+            />
+            <button className="button-play">
+              <span>PLAY NOW</span>
+            </button>
+            </form>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,41 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-transparent  navbar-dark">
+        <div className="container  mx-auto">
+          <a className="navbar-brand fs-2" href="#">
+          PixelPulse
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ms-auto fs-5">
+              <li className="nav-item">
+                <a className="nav-link active " aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://port-folio-aniket-ikhar.vercel.app/">
+                  About Me
+                </a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
 
